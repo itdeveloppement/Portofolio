b48c7fcb47915001c0417926875143c7b0fc2d6d
Marti <118295021+itdeveloppement@users.noreply.github.com>
2024-03-25 14:57:52 +0100

    en cours

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Portofolio
  Presentation projet
+
+# bibliotheques
+
+**swiper**
+https://swiperjs.com/
+Composant name : Slides per view
+Composant : https://codesandbox.io/p/sandbox/tgc8gm?file=%2Findex.html
+
+**menu burger**
+https://github.com/jonsuh/hamburgers?tab=readme-ov-file
+
+
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,44 @@
+// CHANGEMENT TYPO
+document.getElementById("btn-typo").addEventListener("click", changeTypo);
+// changement de la typo du site modal
+let modal = document.querySelector(".header-modal");
+modal.addEventListener("click", changeTypo);
+
+/** change typo du site
+ *  @param {*} // pas de parametre
+ *  @return ne retourne rien
+ */
+function changeTypo() {
+    // ciblage
+    let body = document.querySelector("body");
+    let btnTypo = document.getElementById("btn-typo");
+    // ajout de classe
+    body.classList.toggle("newTypo");
+    btnTypo.classList.toggle("newTypo");
+}
+
+// MODALE HEADER
+
+// ouverture et fermeture burger et modale au click
+document.querySelector(".hamburger").addEventListener("click", ()=> {
+    openModal()
+});
+
+/** ouverture et fermeture burger et modale
+ *  @param {*} // pas de parametre
+ *  @return ne retourne rien 
+ */
+function openModal () {
+    let modale = document.querySelector(".header-modal");
+    modale.classList.toggle("header-modal--is-open");
+    let burger = document.querySelector(".header-burger");
+    burger.classList.toggle("is-active");
+}
+
+
+
+// carousel swiper
+/*
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
@@ -9,3 +50,4 @@ var swiper = new Swiper(".mySwiper", {
   });
 
   console.log("test dans app.js")
+  */
